fa5adad4e040e76e69c055b695ad84d9410ffa4a
jengleberg <jeffengleberg@hotmail.com>
2018-01-27 15:03:31 -0700

    Task Manager

---
+++ main.js
@@ -0,0 +1,86 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+function saveIssue(e) {
+  var issueDesc = document.getElementById('issueDescInput').value;
+  var issuePriority = document.getElementById('issuePriorityInput').value;
+  var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+  var issueId = chance.guid();
+  var issueStatus = 'Open';
+
+  var issue = {
+    id: issueId,
+    description: issueDesc,
+    priority: issuePriority,
+    assignedTo: issueAssignedTo,
+    status: issueStatus
+  }
+
+  if (localStorage.getItem('issues') == null) {
+    var issues = [];
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  } else {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  }
+
+  document.getElementById('issueInputForm').reset();
+
+  fetchIssues();
+
+  e.preventDefault();
+}
+
+function setStatusClosed(id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+
+  for (var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues[i].status = 'Closed';
+    }
+  }
+
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  fetchIssues();
+}
+
+function deleteIssue(id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+
+  for (var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues.splice(i, 1);
+    }
+  }
+
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  fetchIssues();
+}
+
+function fetchIssues() {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+  var issuesListe = document.getElementById('issuesList');
+
+  issuesList.innerHTML = '';
+
+  for (var i = 0; i < issues.length; i++) {
+    var id = issues[i].id;
+    var desc = issues[i].description;
+    var priority = issues[i].priority;
+    var assignedTo = issues[i].assignedTo;
+    var status = issues[i].status;
+
+    issuesList.innerHTML +=   '<div class="card-body bg-light">'+
+                              '<h6>Issue ID: ' + id + '</h6>'+
+                              '<p><span class="badge badge-info">' + status + '</span></p>'+
+                              '<h3>' + desc + '</h3>'+
+                              '<p><span class="fas fa-clock"></span> ' + priority + '</p>'+
+                              '<p><span class="fas fa-user"></span> ' + assignedTo + '</p>'+
+                              '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a> '+
+                              '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
+                              '</div>';
+  }
+}
